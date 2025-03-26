1a0c9588179b6755fd8a905ece3fe6e5590bdc7b
sabsiru <zero013@naver.com>
2025-03-26 21:00:24 +0900

    포인트 충전 테스트
    충전 성공
    최대 한도 초과 시 예외
    포인트 충전값이 음수일시 예외
    포인트 충전값이 0일시 예외

---
+++ src/test/java/io/hhplus/tdd/point/UserPointTest.java
@@ -0,0 +1,67 @@
+package io.hhplus.tdd.point;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class UserPointTest {
+
+    @Test
+    public void 충전_성공() throws Exception {
+        //given
+        UserPoint userPoint = new UserPoint(1L, 1000L, System.currentTimeMillis());
+
+        //when
+        UserPoint after = userPoint.charge(userPoint, 2000L);
+
+        //then
+        assertEquals(after.point(), 3000L);
+
+    }
+
+    @Test
+    public void 충전시_최대한도_초과시_예외발생() throws Exception {
+        //given
+        UserPoint userPoint = new UserPoint(1L, 90000L, System.currentTimeMillis());
+
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> UserPoint.charge(userPoint, 10001L)
+        );
+
+        //then
+        assertEquals("충전 가능한 최대 포인트는 100,000입니다.", e.getMessage());
+
+    }
+
+    @Test
+    public void 충전포인트가_음수일시_예외발생() {
+        //given
+        UserPoint userPoint = new UserPoint(1L, 5000L, System.currentTimeMillis());
+
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> userPoint.charge(userPoint, -1000L)
+        );
+
+        //then
+        assertEquals("충전 금액은 1 이상이어야 합니다.", e.getMessage());
+    }
+
+    @Test
+    public void 충전포인트가_0일시_예외발생() {
+        //given
+        UserPoint userPoint = new UserPoint(1L, 5000L, System.currentTimeMillis());
+
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> userPoint.charge(userPoint, 0)
+        );
+
+        //then
+        assertEquals("충전 금액은 1 이상이어야 합니다.", e.getMessage());
+    }
+}
