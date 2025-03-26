fdae55c559fab93d4d7d30efa07917284cb0ab81
sabsiru <zero013@naver.com>
2025-03-26 20:56:17 +0900

    포인트 충전
    포인트 충전은 도메인에 할당하고
    예외 처리 작성

--- src/main/java/io/hhplus/tdd/point/UserPoint.java
+++ src/main/java/io/hhplus/tdd/point/UserPoint.java
@@ -9,4 +9,15 @@ public record UserPoint(
     public static UserPoint empty(long id) {
         return new UserPoint(id, 0, System.currentTimeMillis());
     }
+
+    public static UserPoint charge(UserPoint current, long amount) {
+        if(amount <= 0) {
+            throw new IllegalArgumentException("충전 금액은 1 이상이어야 합니다.");
+        }
+        long newPoint = current.point() + amount;
+        if (newPoint > 100_000L) {
+            throw new IllegalArgumentException("충전 가능한 최대 포인트는 100,000입니다.");
+        }
+        return new UserPoint(current.id(), newPoint, System.currentTimeMillis());
+    }
 }
