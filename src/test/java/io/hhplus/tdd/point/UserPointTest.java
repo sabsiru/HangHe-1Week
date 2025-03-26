629dbe968e3b9da610c718c96cc9be56ddac9146
sabsiru <zero013@naver.com>
2025-03-26 21:11:02 +0900

    포인트 사용 테스트
    사용 성공
    사용시 남은 포인트가 0일시 예외
    포인트 사용값이 음수일시 예외
    포인트 사용값이 0일시 예외

--- src/test/java/io/hhplus/tdd/point/UserPointTest.java
+++ src/test/java/io/hhplus/tdd/point/UserPointTest.java
@@ -64,4 +64,60 @@ class UserPointTest {
         //then
         assertEquals("충전 금액은 1 이상이어야 합니다.", e.getMessage());
     }
+
+    @Test
+    public void 사용_성공() throws Exception {
+        //given
+        UserPoint userPoint = new UserPoint(1L, 5000L, System.currentTimeMillis());
+
+        //when
+        UserPoint after = userPoint.use(userPoint, 2000L);
+
+        //then
+        assertEquals(after.point(), 3000L);
+
+    }
+
+    @Test
+    public void 사용시_남은_포인트가_0미만이면_예외발생() throws Exception {
+        //given
+        UserPoint userPoint = new UserPoint(1L, 5000L, System.currentTimeMillis());
+
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> UserPoint.use(userPoint, 5001L)
+        );
+
+        //then
+        assertEquals("포인트가 부족합니다.", e.getMessage());
+
+    }
+
+    @Test
+    public void 사용_포인트가_음수일시_예외발생() {
+        //given
+        UserPoint userPoint = new UserPoint(1L, 5000L, System.currentTimeMillis());
+
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> userPoint.use(userPoint, -1000L)
+        );
+
+        //then
+        assertEquals("사용 금액은 1 이상이어야 합니다.", e.getMessage());
+    }
+
+    @Test
+    public void 사용_포인트가_0일시_예외발생() {
+        //given
+        UserPoint userPoint = new UserPoint(1L, 5000L, System.currentTimeMillis());
+
+        //when
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class,
+                () -> userPoint.use(userPoint, 0)
+        );
+
+        //then
+        assertEquals("사용 금액은 1 이상이어야 합니다.", e.getMessage());
+    }
 }
